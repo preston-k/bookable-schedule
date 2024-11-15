598ebd10e609832bbce5d43250cc41552a57de7a
Preston Kwei <prestonkwei@gmail.com>
2024-11-14 23:28:59 -0800

    calendar updates and moving back and forth

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const firebaseConfig = {
   projectId: 'bookableschedule',
   storageBucket: 'bookableschedule.appspot.com',
   messagingSenderId: '891298873089',
-  appId: '1:891298873089:web:de053844e86322f8456500'
+  appId: '1:891298873089:web:de053844e86322f8456500',
 }
 firebase.initializeApp(firebaseConfig)
 let database = firebase.database()
@@ -19,16 +19,67 @@ const urlParams = new URLSearchParams(window.location.search)
 
 let monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 let currentMonth = new Date().getMonth()
-let date 
-let month = new Date().getMonth() + 1
+let currentYear = new Date().getFullYear()
+let date
+let month = currentMonth + 1
 let monthWord = monthNames[currentMonth]
-let year = new Date().getFullYear()
+let year = currentYear
 let info
 let to
 let from
 document.querySelector('#Df8m').innerHTML = monthWord + ' ' + year
 let daysInMonth = dayCount(year, month)
-let id=urlParams.get('id')
+let id = urlParams.get('id')
+let max = 10
+document.querySelector('#back').addEventListener('click', () => {
+  if (max > 6) {
+    max -= 1
+    currentMonth--
+    if (currentMonth < 0) {
+      currentMonth = 11
+      currentYear--
+    }
+    updateCalendar()
+  } else {
+    alert("Sorry, you can't go back that far!")
+  }
+})
+
+document.querySelector('#forward').addEventListener('click', () => {
+  if (max < 16) {
+    max += 1
+    currentMonth++
+    if (currentMonth > 11) {
+      currentMonth = 0
+      currentYear++
+    }
+    updateCalendar()
+  } else {
+    alert("Sorry, you can't go that far ahead.")
+  }
+})
+
+function clearCalendar() {
+  for (let weekNum = 0; weekNum < 5; weekNum++) {
+    for (let day = 0; day < 7; day++) {
+      const outerElement = document.querySelector(`#w${weekNum + 1}d${day + 1}-outer`)
+      const innerElement = document.querySelector(`#w${weekNum + 1}d${day + 1}-inner`)
+      outerElement.classList.remove('today')
+      outerElement.classList.remove('shadow-hide')
+    }
+  }
+}
+
+function updateCalendar() {
+  month = currentMonth + 1
+  monthWord = monthNames[currentMonth]
+  year = currentYear
+  document.querySelector('#Df8m').innerHTML = monthWord + ' ' + year
+  daysInMonth = dayCount(year, month)
+  clearCalendar()
+  checkInvite()
+}
+
 async function checkInvite() {
   if (urlParams.get('id') == '' || urlParams.get('id') == null) {
     accessDenied()
@@ -36,7 +87,7 @@ async function checkInvite() {
     const snapshot = await database.ref('/invites').once('value')
     let inviteFound = false
 
-    snapshot.forEach(childSnapshot => {
+    snapshot.forEach((childSnapshot) => {
       if (childSnapshot.key == urlParams.get('id')) {
         inviteFound = true
       }
@@ -44,29 +95,26 @@ async function checkInvite() {
 
     let suggestList
     if (inviteFound) {
-      
       const inviteSnapshot = await database.ref(`/invites/${urlParams.get('id')}/`).once('value')
 
       setTimeout(() => {
         document.querySelector('#loader-wrap').style.display = 'none'
-      }, '875') // Save time in case extra things load not in an await
+      }, '875')
       info = inviteSnapshot.val()
-      console.log(info['hide'])
       if (info['hide'] == true) {
-        document.querySelector('#used').style.display='flex'
+        document.querySelector('#used').style.display = 'flex'
         document.querySelector('#main-calendar').innerHTML = ''
       }
       to = info['details']['to']
       from = info['details']['from']
-      console.log(info)
-      Object.values(info.suggest).forEach(obj => {
-        console.log(obj)
+      suggestList = ''
+      Object.values(info.suggest).forEach((obj) => {
         suggestList += `<a class='link-lruhj' href='/book.html?passthrough=${id}&action=book&passfrom=${obj.id}'><div class='suggested-timebox'> <div class='fbsyq'> <span class='material-symbols-outlined'> arrow_forward_ios </span> </div> <div class='suggested-timebox-date'> <img src='https://cdn.prestonkwei.com/calendaricon-black-small.png' class='img-qi5s' alt='Calendar Icon' draggable='false'> <p><span>${obj.date}</span></p> </div> <div class='suggested-timebox-time'> <img src='https://cdn.prestonkwei.com/clockicon-black-small.png' class='img-4od9' alt='Clock Icon' draggable='false'> <p><span>${obj.time}</span></p> </div> </div></a>`
       })
       suggestList = suggestList.replace(/undefined/g, '')
       document.querySelector('#pdufa').innerHTML = suggestList
-      document.querySelectorAll('.sendname').forEach(element => element.innerHTML = to)
-      document.querySelectorAll('.refname').forEach(element => element.innerHTML = from)
+      document.querySelectorAll('.sendname').forEach((element) => (element.innerHTML = to))
+      document.querySelectorAll('.refname').forEach((element) => (element.innerHTML = from))
       date = new Date().toISOString().split('T')[0]
       let i = 1
       let monthstart = new Date(`${monthWord} 1, ${year} 12:00:00`).getDay()
@@ -95,6 +143,25 @@ async function checkInvite() {
             outerElement.classList.add('shadow-box')
           } else {
             innerElement.innerHTML = weeks[weekNum][day]
+
+            // Add class for the first column
+            if (day === 0) {
+              outerElement.classList.add('bl-eznf3')
+            }
+
+            // Add class for the bottom row
+            if (weekNum === 4) {
+              outerElement.classList.add('br-6c6u9')
+            }
+
+            const currentDate = new Date()
+            const currentDay = new Date(year, month - 1, weeks[weekNum][day])
+
+            if (currentDate.getDate() === weeks[weekNum][day] && currentDate.getMonth() + 1 === month && currentDate.getFullYear() === year) {
+              outerElement.classList.add('today')
+            } else if (currentDay < currentDate) {
+              outerElement.classList.add('shadow-hide')
+            }
           }
         }
       }
--- send.js
+++ send.js
@@ -4,7 +4,7 @@ const firebaseConfig = {
   projectId: 'bookableschedule',
   storageBucket: 'bookableschedule.appspot.com',
   messagingSenderId: '891298873089',
-  appId: '1:891298873089:web:de053844e86322f8456500'
+  appId: '1:891298873089:web:de053844e86322f8456500',
 }
 firebase.initializeApp(firebaseConfig)
 function tree(obj, prefix = '') {
@@ -14,9 +14,9 @@ function tree(obj, prefix = '') {
   keys.forEach((key, index) => {
     const isLast = index === keys.length - 1
     const value = typeof obj[key] === 'object' && obj[key] !== null ? '' : `: ${obj[key]}`
-    
+
     html += `<p class='tree-es1rn'>${prefix}${isLast ? '└── ' : '├── '}${key}${value}</p>`
-    
+
     if (typeof obj[key] === 'object' && obj[key] !== null) {
       html += generateObjectTreeHTML(obj[key], `${prefix}${isLast ? '&nbsp;&nbsp;&nbsp;&nbsp;' : '|&nbsp;&nbsp;&nbsp;'}`)
     }
@@ -25,12 +25,10 @@ function tree(obj, prefix = '') {
   return html
 }
 
-
-
 let offset = -(new Date().getTimezoneOffset() / 60)
 let database = firebase.database()
-let inviteid = crypto.randomUUID() 
-let details = {'id':inviteid}
+let inviteid = crypto.randomUUID()
+let details = { id: inviteid }
 let suggestions
 let suggestList = {}
 document.querySelector('#h5j3l').innerHTML = inviteid
@@ -53,14 +51,14 @@ document.querySelector('#suggest-date-time').addEventListener('submit', (event) 
   const formattedTime = `${hours}:${minutes.toString().padStart(2, '0')} ${period}`
   console.log(formattedTime)
   console.log(formattedDate)
-  let curList = document.querySelector('#suggested-timeslist').innerHTML 
+  let curList = document.querySelector('#suggested-timeslist').innerHTML
   newlist = curList += `<div class='suggest-preview'><span class='suggest-date'>${formattedDate}</span> @ <span class='suggest-time'>${formattedTime}</span></div>`
   document.querySelector('#suggested-timeslist').innerHTML = newlist
   database.ref(`/invites/${inviteid}/suggest/${suggestId}`).update({
     date: formattedDate,
     time: formattedTime,
     off: offset,
-    id: suggestId
+    id: suggestId,
   })
   document.querySelector('#input-2h940').value = ''
   document.querySelector('#input-xp65i').value = ''
@@ -87,15 +85,15 @@ document.querySelector('#verify-rkyev').addEventListener('click', async () => {
   database.ref(`/invites/${inviteid}/`).update({
     details,
     ts: new Date(),
-    hide: false
+    hide: false,
   })
-  document.querySelector('#verify-wrap').style.display='none'
+  document.querySelector('#verify-wrap').style.display = 'none'
   const snapshot = await database.ref(`/invites/${inviteid}/`).once('value')
   console.log(snapshot.val().suggest)
-  Object.values(snapshot.val().suggest).forEach(obj => {
+  Object.values(snapshot.val().suggest).forEach((obj) => {
     if (obj != undefined && obj != null) {
       console.log(obj.id)
-      invitelist += `<a href='https://schedule.prestonkwei.com/book.html?passthrough=${inviteid}&action=book&passfrom=${obj.id}' style='text-decoration: none; color: black; all: unset; display: block;'> <div style='width: 100%; border: 2px solid black; border-radius: 10px; position: relative; cursor: pointer; transition: transform 0.2s ease-out; margin-bottom: 15px;'> <div style='display: flex; align-items: center; height: 50px;'> <img src='https://cdn.prestonkwei.com/calendaricon-black-small.png' alt='Calendar Icon' draggable='false' style='width: 50px; height: 50px; border-right: 2px solid black; margin-right: 5px;'> <p><span>${obj.date}</span></p> </div> <div style='display: flex; align-items: center; height: 50px;'> <img src='https://cdn.prestonkwei.com/clockicon-black-small.png' alt='Clock Icon' draggable='false' style='width: 50px; height: 50px; border-right: 2px solid black; margin-right: 5px;'> <p><span>${obj.time}</span></p> </div> </div> </a>`
+      invitelist += `<a href="https://schedule.prestonkwei.com/book.html?passthrough=${inviteid}&action=book&passfrom=${obj.id}" style="text-decoration: none; color: black; display: block; all: unset"> <table style="width: 100%; border: 2px solid black; border-radius: 10px; margin-bottom: 15px; cursor: pointer"> <tr> <td style="vertical-align: top; width: 50px; padding: 5px; border-right: 2px solid black"> <img src="https://cdn.prestonkwei.com/calendaricon-black-small.png" alt="Calendar Icon" draggable="false" style="width: 50px; height: 50px" /> </td> <td style="padding: 5px"> <p>${obj.date}</p> </td> <td rowspan="2" style="width: 75px; vertical-align: middle; text-align: center"> <img src="https://cdn.prestonkwei.com/chevron-right-50-black.png" alt="Go to Booking" style="width: 75px; height: 75px" /> </td> </tr> <tr> <td style="vertical-align: top; width: 50px; padding: 5px; border-right: 2px solid black"> <img src="https://cdn.prestonkwei.com/clockicon-black-small.png" alt="Clock Icon" draggable="false" style="width: 50px; height: 50px" /> </td> <td style="padding: 5px"> <p>${obj.time}</p> </td> </tr> </table> </a>`
     }
   })
   console.log(invitelist)
@@ -103,14 +101,8 @@ document.querySelector('#verify-rkyev').addEventListener('click', async () => {
   data.set('sendto', `${document.querySelector('#input-8tka6').value}`)
   data.set('from', document.querySelector('#input-csc2e').value)
   data.set('subject', `${document.querySelector('#input-5ytqq').value} has suggested meeting sometime soon!`)
-  data.set(
-    'html',
-    `<!DOCTYPE html> <html lang='en'> <head> <meta charset='UTF-8'> <meta http-equiv='X-UA-Compatible' content='IE=edge'> <meta name='viewport' content='width=device-width, initial-scale=1.0'> </head> <body style='font-family: system-ui, sans-serif; background-color: #7EC8E3; margin: 0; padding: 20px;'> <table width='100%' cellspacing='0' cellpadding='0' border='0' style='background-color: #7EC8E3; padding: 20px;'> <tr> <td align='center'> <table width='600' cellspacing='0' cellpadding='20' border='0' style='background-color: white; border-radius: 8px;'> <tr></tr> <tr> <td style='font-size: 16px; line-height: 1.6;'> <p>Hi, ${document.querySelector('#input-m07mt').value}!</p><p>${document.querySelector('#input-5ytqq').value} would like to meet with you, and has suggested the following times.</p> ${invitelist} <p>Prefer a different time? View <a href='https://schedule.prestonkwei.com/?id=${inviteid}'>${document.querySelector('#input-5ytqq').value}'s availability here.</a></td> </tr> <tr> <td style='font-family: system-ui, sans-serif; font-size: 10px; text-align: center;'> <hr> <p>This email was sent on behalf of ${document.querySelector('#input-csc2e').value} who sent you this invite.</p> <p>PrestonKwei.com ⋅ PO Box 20987 ⋅ Oakland, CA 94620</p> <p>This is an unmonitored email address. Responses will not be received.</p> <a href='https://prestonkwei.com'><p>prestonkwei.com</p></a> </td> </tr> </table> </td> </tr> </table> </body> </html>`.replace(/undefined/g, '')
-  )
-  data.set(
-    'content',
-    `Sorry, this email didn't format correctly. Please contact the person who sent you this for help and more information. You can delete this email (or keep it, we won't mind).\n\nERR: html fail to content.`
-  )
+  data.set('html', `<!DOCTYPE html> <html lang='en'> <head> <meta charset='UTF-8'> <meta http-equiv='X-UA-Compatible' content='IE=edge'> <meta name='viewport' content='width=device-width, initial-scale=1.0'> </head> <body style='font-family: system-ui, sans-serif; background-color: #7EC8E3; margin: 0; padding: 20px;'> <table width='100%' cellspacing='0' cellpadding='0' border='0' style='background-color: #7EC8E3; padding: 20px;'> <tr> <td align='center'> <table width='600' cellspacing='0' cellpadding='20' border='0' style='background-color: white; border-radius: 8px;'> <tr></tr> <tr> <td style='font-size: 16px; line-height: 1.6;'> <p>Hi, ${document.querySelector('#input-m07mt').value}!</p><p>${document.querySelector('#input-5ytqq').value} would like to meet with you, and has suggested the following times.</p> ${invitelist} <p>Prefer a different time? View <a href='https://schedule.prestonkwei.com/?id=${inviteid}'>${document.querySelector('#input-5ytqq').value}'s availability here.</a></td> </tr> <tr> <td style='font-family: system-ui, sans-serif; font-size: 10px; text-align: center;'> <hr> <p>This email was sent on behalf of ${document.querySelector('#input-csc2e').value} who sent you this invite.</p> <p>PrestonKwei.com ⋅ PO Box 20987 ⋅ Oakland, CA 94620</p> <p>This is an unmonitored email address. Responses will not be received.</p> <a href='https://prestonkwei.com'><p>prestonkwei.com</p></a> </td> </tr> </table> </td> </tr> </table> </body> </html>`.replace(/undefined/g, ''))
+  data.set('content', `Sorry, this email didn't format correctly. Please contact the person who sent you this for help and more information. You can delete this email (or keep it, we won't mind).\n\nERR: html fail to content.`)
   fetch('https://emailserver.prestonkwei.com/email', {
     method: 'post',
     body: data,
